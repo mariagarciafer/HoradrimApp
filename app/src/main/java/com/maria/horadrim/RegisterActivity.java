54e6636d82696d27f5e46caefbc1c25e1da7eaa5
María García Fernández <mdelgf07@educastur.es>
2022-11-12 19:43:03 +0100

    Initial commit

---
+++ app/src/main/java/com/maria/horadrim/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.maria.horadrim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
